3492d877b642c3c63e05f6953965c950dc2933a8
Michael Sharman <michael@thecodesharman.com.au>
2025-06-09 00:39:00 +1000

    Attemtpt to improve decoder
    I noticed that the 4 bits lead in vs 3 bits lead in isn;t enough because
    in `ts_GetCommnand` pulses 5 times then reads 8 bits to read a command.
    
    The comments call this a RD operation but I don't understand how this
    fits into the 4 bit lead in also labelled RD earlier so we add a
    seperate case.

--- decoders/acorn_post/pd.py
+++ decoders/acorn_post/pd.py
@@ -9,7 +9,7 @@ class Decoder(srd.Decoder):
     license = 'gplv2+'
     inputs = ['logic']
     outputs = []
-    tags = ['Embedded/insdustrial']
+    tags = ['Embedded/industrial']
     channels = (
         {'id': 'a23', 'name': 'A23', 'desc': 'A23 lines'},
     )
@@ -26,16 +26,12 @@ class Decoder(srd.Decoder):
         self.reset()
 
     def reset(self):
-        self.state = 'idle'
-        self.bit_start = None
-        self.bit_value = None
-        self.operation_type = None
-        self.pulse_count = 0
+        pass
    
     def start(self):
         self.out_ann = self.register(srd.OUTPUT_ANN)
 
-    def read_write_command_value(self):
+    def read_byte(self):
         value = 0
         for bit in range(8):
             start = self.samplenum
@@ -53,20 +49,40 @@ class Decoder(srd.Decoder):
                 value = (value << 1) | 1
         return value
     
-    def read_or_write_command(self):
+    def decode_adapter_operation(self):
         start = self.samplenum
-        for _ in range(3):
-            self.wait({0: 'r'})
-            self.wait({0: 'f'})
-        self.wait([{0: 'r'}, {'skip': 10}])
-  
-        if (self.matched == 1):
-            self.wait({0: 'f'})
-            self.put(start, self.samplenum, self.out_ann, [1, ['RD', 'R']])
-        else:
-             value = self.read_write_command_value()
-             self.put(start, self.samplenum, self.out_ann, [1, ['WS: ' + hex(value), 'WS', 'W']])
+        pulse_count = 0
+        counting = True
 
+        # Count the number of consecutive pulses
+        while counting || pulse_count < 5:
+            self.wait({0: 'r'}, {'skip': 3})
+            if (self.matched == 1):
+                self.wait({0: 'f'})
+                pulse_count += 1
+            else:
+                counting = False
+             
+        # Depending on how many pulses we get we can determine what should follow
+        match pulse_count:
+            case 4: # RD command
+                self.put(start, self.samplenum, self.out_ann, [1, ['RD', 'R']])
+            case 3: # WS command
+                value = self.read_byte()
+                self.put(start, self.samplenum, self.out_ann, [1, ['WS: ' + hex(value), 'WS', 'W']])
+            case 2 | 1: 
+                # This shouldn't happen unless we're out of sync with the stream
+                # so we just skip a bunch of clocks and try again.
+                self.wait({'skip': 32})
+            case _: 
+                # if get here this is the pulsing to read a command from the POST adapter
+                # but since we're a dummy adapter we just wait for the rest of the 8 bits
+                # to pulse.
+                for _ in range(7):
+                    self.wait({0: 'r'})
+                    self.wait({0: 'f'})
+                self.put(start, self.samplenum, self.out_ann, [1, ['Get Command', 'GC', 'G']])
+                
     def decode(self):
         while True:
-            self.read_or_write_command()
+            self.decode_adapter_operation()
