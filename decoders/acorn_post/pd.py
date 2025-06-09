74ae3c24fa8d8d89f08a99c3ffed748b9e40486d
Michael Sharman <thecodesharman@gmail.com>
2025-06-09 13:25:50 +1000

    make decode_input accepts 0 bit reads

--- decoders/acorn_post/pd.py
+++ decoders/acorn_post/pd.py
@@ -19,65 +19,79 @@ class Decoder(srd.Decoder):
         ('in_bits', 'Input bits'),
         ('read', 'Read'),
         ('write', 'Write'),
-        ('warnings', 'HUman readable warnings')
+        ('warnings', 'Human readable warnings')
     )
     annotation_rows = (
          ('bits', 'Bits', (0,1)),
-         ('commands', 'Commands', (2,3,4)),
+         ('commands', 'Commands', (2,3)),
+         ('warnings', 'Warnings', (4,))
      )
 
     def __init__(self):
-        self.interbit_interval = 164 * self.samplerate  # in microseconds, as per the protocol spec
-        self.reset()
+       self.reset()
 
     def reset(self):
         pass
    
     def start(self):
         self.out_ann = self.register(srd.OUTPUT_ANN)
+        
+        # Use existing rate or default to 20MHz
+        self.samplerate = getattr(self, 'samplerate', 20_000_000)
+        self.pulse_duration = (self.samplerate // 1_000_000) * 4
+        self.interbit_interval = (self.samplerate // 1_000_000) * 164
 
-    # Count the number of pulses within 164μS window
+    # Look for a burst of pulses within 164μS window
     def count_pulses(self):
         pulse_count = 0
-
+        self.wait({0: 'l'})
+        self.wait([{0: 'r'}, {'skip': self.interbit_interval}])
+        start = self.samplenum
+        if self.matched == 2:
+            return (0, start)
+        else:
+            pulse_count += 1
+        
         # Count the number of consecutive pulses
-        while pulse_count <= 4:
-            self.wait([{0: 'r'}, {'skip': self.interbit_interval}])
+        while pulse_count < 4:
+            self.wait([{0: 'r'}, {'skip': self.pulse_duration}])
             if (self.matched == 1): # WTF: shouldn't this be self.matched == (True, False)?
                 self.wait({0: 'f'})
                 pulse_count += 1
             else:
                 break
-
-        return pulse_count
+        print(f"pulse_count: {pulse_count}")
+        return (pulse_count, start)
 
     def decode_output_byte(self):
         value = 0
         for bit in range(8):
-            start = self.samplenum
-
-            match self.count_pulses():
+            pulse_count, start = self.count_pulses()
+            match pulse_count:
                 case 1:
                     self.put(start, self.samplenum, self.out_ann, [0, ['1']])
                     value = value | 1
                 case 2:
                     self.put(start, self.samplenum, self.out_ann, [0, ['0']])
-
+                    value = value | 0
+                case 0:
+                    self.put(start, self.samplenum, self.out_ann, [4, ['Invalid bits', 'INV', 'IN', 'I']])
+                    return 0
             value = value << 1
         return value
         
     def decode_input(self):
         # wait for adapter to acknowledge read request
-        self.wait([{0: 'r', 1: 'h'}, {'skip': self.interbit_interval}])
+        self.wait([{0: 'h', 1: 'h'}, {'skip': self.pulse_duration}])
         if self.matched == 2:
-            self.wait({0: 'f'})
             return 0
 
         value = 0
-        bits_recieved = 0
+        bits_received = 0
 
         while bits_received <= 32:
-            self.wait({0: 'r'}, {'skip': self.interbit_interval})
+            start = self.samplenum
+            self.wait([{0: 'r'}, {'skip': self.pulse_duration}])
             if self.matched == 1:
                 a23, d0 = self.wait({0: 'f'})
                 bits_received += 1
@@ -90,13 +104,16 @@ class Decoder(srd.Decoder):
     
     def decode_adapter_operation(self):
         start = self.samplenum
-        match self.count_pulses():
+        pulse_count, start = self.count_pulses()
+        match pulse_count:
             case 4:
                 value = self.decode_input()
-                self.put(start, self.samplenum, self.out_ann, [1, ['Input: ' + hex(value), 'IN', 'R']])
+                self.put(start, self.samplenum, self.out_ann, [2, ['Input: ' + hex(value), 'IN', 'R']])
             case 3:
                 value = self.decode_output_byte()
-                self.put(start, self.samplenum, self.out_ann, [0, ['Output: ' + hex(value), 'OUT', 'O']])
+                self.put(start, self.samplenum, self.out_ann, [3, ['Output: ' + hex(value), 'OUT', 'O']])
+            case 0:
+                pass
             case _: 
                 self.put(start, self.samplenum, self.out_ann, [4, ['Invalid bits', 'INV', 'IN', 'I']])
                 
