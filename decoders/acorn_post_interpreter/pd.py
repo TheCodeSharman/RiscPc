a4e993ab81f1c2302a7752e9743f5e8da230a1f2
Michael Sharman <thecodesharman@gmail.com>
2025-06-09 17:03:37 +1000

    Begining of interpeting the LCD commands

--- decoders/acorn_post_interpreter/pd.py
+++ decoders/acorn_post_interpreter/pd.py
@@ -12,9 +12,11 @@ class Decoder(srd.Decoder):
     tags = ['Debug/trace']
     annotations = (
         ('getcommand', 'GetCommand'),
+        ('sendtext', 'SendText'),
+        ('lcdcmd', 'LCD Command'),
     )
     annotation_rows = (
-        ('commands', 'Commands', (0,)),
+        ('commands', 'Commands', (0,1,2)),
     )
 
     def __init__(self):
@@ -23,6 +25,10 @@ class Decoder(srd.Decoder):
     def reset(self):
         self.command_buffer = []
         self.sequence_start = None
+        self.last_getcommand = None
+        self.text_buffer = []
+        self.text_start = None
+        self.last_lcd_output = None  # Track the last LCD output command
 
     def start(self):
         self.out_ann = self.register(srd.OUTPUT_ANN)
@@ -43,8 +49,43 @@ class Decoder(srd.Decoder):
             # Output the interpreted GetCommand
             self.put(start_sample, end_sample, self.out_ann,
                     [0, [f'GetCommand: cmd={cmd_num}, param={param}']])
+            
+            # Store the last GetCommand for SendText detection
+            self.last_getcommand = (cmd_num, param)
             return True
         return False
+
+    def decode_sendtext(self, startsample, endsample, data):
+        cmd_type, value = data
+        
+        # Handle input acknowledgment for previous LCD output
+        if cmd_type == 'input' and self.last_lcd_output is not None:
+            # Input acknowledgment received, clear the last output
+            self.last_lcd_output = None
+            return True
+            
+        if cmd_type != 'output':
+            return False
+            
+        # Store the start of the text sequence
+        if self.text_start is None:
+            self.text_start = startsample
+            
+        # Check if this is a print command (MSB = 1000)
+        if (value & 0xF0) == 0x80:
+            # This is a print command, the LSB is the character
+            char = value & 0x0F
+            self.put(self.text_start, endsample, self.out_ann,
+                    [1, [f'Print: {char:02X}h']])
+        else:
+            # This is an LCD control command
+            self.put(self.text_start, endsample, self.out_ann,
+                    [2, [f'LCD: {value:02X}h']])
+        
+        # Store this output command to wait for its input acknowledgment
+        self.last_lcd_output = (startsample, endsample, value)
+        self.text_start = None
+        return True
 
     def decode(self, startsample, endsample, data):
         if not data:
@@ -52,7 +93,18 @@ class Decoder(srd.Decoder):
             
         cmd_type, value = data
         
-        # Store the command in our buffer
+        # If we're expecting SendText (after GetCommand 0 or 31)
+        if self.last_getcommand:
+            cmd_num, _ = self.last_getcommand
+            if cmd_num in (0, 31):  # LCD display or dummy adapter
+                if self.decode_sendtext(startsample, endsample, data):
+                    return
+                # If we get here, it wasn't a SendText command, so reset the state
+                self.last_getcommand = None
+                self.text_buffer = []
+                self.text_start = None
+            
+        # Try to decode as GetCommand
         self.command_buffer.append((startsample, endsample, cmd_type, value))
         
         # If this is the first command in a potential sequence, record its start
